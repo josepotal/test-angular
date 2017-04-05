ef5c45c33f1064e051fdfd2bd183a908a570792d
Josep Otal <j.otal.pares@gmail.com>
2017-04-05 23:53:47 +0200

    basic functionality done

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -11,11 +11,21 @@
         //var vm = this;
         $scope.message = 'Ejemplo sencillo para comprobaciones';
 
-        MainFactory.getCountries()
-          .then(function(response){
-              $scope.allCountries = response
-          })
+        $scope.locations = MainFactory.locationsList
 
+        //default starting values
+        $scope.nameSelected = $scope.locations[0].name
+        $scope.imgSelected = $scope.locations[0].img
+        $scope.latSelected = $scope.locations[0].lat
+        $scope.longSelected = $scope.locations[0].long
+
+        // After selecting a location
+        $scope.getLocation = function(event, location){
+            $scope.nameSelected = location.name
+            $scope.imgSelected = location.img
+            $scope.latSelected = location.lat
+            $scope.longSelected = location.long
+        };
 
     }
 })();
